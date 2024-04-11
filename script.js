41b8613fe861d25477bfeb71547459a7608f47b5
mhkmhsh <mahekmahesh12@gmail.com>
2024-04-11 11:12:46 -0400

    ghjhj

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const namesArray = []
 function addName() {
     const nameInput = document.getElementById('nameInput') //storing name input element as a var
     const name = nameInput.value.trim() //get the TRIMMED value of the input box and store in a var
-
+ if (name === '') {
+        alert('please enter a name')
+        
     namesArray.push(name)//add the name to the end of the array
     displayNames() // Call the displayNames function to update the list
 
     nameInput.value = '' // Clear the input field after adding the name
+   
 }
 
 
